a9fba3b214205e1f3db4e83a29b0b540b377317d
Hardinah RG <hardinah.rajaonanirina@gmail.com>
2024-11-12 19:51:24 +0300

    refactor(controller) : change some line to setup and refactor for using REST API

--- src/Controller/RateComparisonController.php
+++ src/Controller/RateComparisonController.php
@@ -2,11 +2,12 @@
 
 namespace App\Controller;
 
-use App\Form\RateComparisonType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Form\RateComparisonType;
 use App\Service\RateComparisonService;
 
 class RateComparisonController extends AbstractController
@@ -28,5 +29,20 @@ class RateComparisonController extends AbstractController
         return $this->render('rate_comparison/index.html.twig', [
             'form' => $form->createView(),
         ]);
+    }
+
+    #[Route('/api/compare', name: 'api_rate_comparison', methods: ['POST'])]
+    public function apiCompare(Request $request, RateComparisonService $rateService): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+
+        if (!$data || !isset($data['loan_amount']) || !isset($data['loan_duration'])) {
+            return new JsonResponse(['error' => 'Invalid data: loan_amount and loan_duration are required'], JsonResponse::HTTP_BAD_REQUEST);
+        }
+
+        // Get by using service
+        $offers = $rateService->getOffers($data);
+
+        return new JsonResponse($offers, JsonResponse::HTTP_OK);
     }
 }
